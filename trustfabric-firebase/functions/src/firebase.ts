63769cb0caaff8017fda22995a19bbebf348b1de
PranjalPoudel03 <pranjalpoudel2002@gmail.com>
2026-06-14 11:13:04 -0500

    Sync branch with main

--- trustfabric-firebase/functions/src/firebase.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from 'firebase-admin';
-import * as serviceAccount from './service-firebase.json';
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
-});
-
-export const db = admin.firestore();
